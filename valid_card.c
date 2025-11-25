b43226c223ddf0d028330268411b2849b98b44a3
Pragyan124 <pragyanborthakur14@gmail.com>
2025-11-25 21:00:15 +0530

    v2

--- valid_card.c
+++ valid_card.c
@@ -2,37 +2,37 @@
 
 /* Luhn's Algorithm Implementation to check if a credit card  number  is valid*/
 
-int validCard(long long cardNumber){
+int validCard(long long card_number){
 
-    long long temp = cardNumber;
-    int countdigit = 0; 
+    long long temp = card_number;
+    int count_digit = 0; 
     int digit_sum = 0;
 
     while(temp > 0){
-        countdigit++;
+        count_digit++;
         temp = temp / 10;
     }
     
-    //Initialize an storedigitay to store the digits of the number
-    int storedigit[countdigit];
+    //Initialize an store_digitay to store the digits of the number
+    int store_digit[count_digit];
 
-    //Add digits of the number to the storedigitay
-    for(int i = countdigit - 1; i >= 0; i--){
-        storedigit[i] = cardNumber % 10;
-        cardNumber = cardNumber / 10;
+    //Add digits of the number to the store_digitay
+    for(int i = count_digit - 1; i >= 0; i--){
+        store_digit[i] = card_number % 10;
+        card_number = card_number / 10;
     }
 
-    for(int i = countdigit - 1; i >= 0; i--){
+    for(int i = count_digit - 1; i >= 0; i--){
         if(i % 2 == 0){
-            storedigit[i] = storedigit[i] * 2;
-            if(storedigit[i] > 9){
-                storedigit[i] = storedigit[i] - 9;
+            store_digit[i] = store_digit[i] * 2;
+            if(store_digit[i] > 9){
+                store_digit[i] = store_digit[i] - 9;
             }
         }
     }
 
-    for(int i = 0; i < countdigit; i++){
-        digit_sum += storedigit[i];
+    for(int i = 0; i < count_digit; i++){
+        digit_sum += store_digit[i];
     }
 
 
